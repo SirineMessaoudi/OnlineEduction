8fa21ba546ed4f09765a251afb9c07e4bfee97a5
SirineMessaoudi <cirinemessaoudi07@gmail.com>
2022-01-18 00:21:06 +0100

    first commit

---
+++ src/main/java/com/example/demo/OnlinePlateformeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePlateformeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePlateformeApplication.class, args);
+	}
+
+}
